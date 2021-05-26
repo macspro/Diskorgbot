fb09bb3e3d6ef89d715b6e83d41aa58b8467453a
macspro <61458872+macspro@users.noreply.github.com>
2021-05-26 08:06:32 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: ython discord-bot.py
---
+++ discord-bot.py
@@ -0,0 +1,102 @@
+import os
+
+import discord
+from discord.ext import commands
+import asyncio
+from discord.utils import get
+
+bye = ["bye", "goodbye", "poca", "poka", "пока", "гудбай", "бувай", "допобачення", "досвидания", "астала", "виста", "иди в мут", "я спать", "я пошел", " гуд бай", "астала виста", "я ухожу"]     # создаем список "привет" слов
+
+hello = ["hi", "hello", "privet", "привет", "хай", "халоу", "воц", "ап", "whats", "up", "привіт", "салам", "вос", "ап", "доброго времени суток", "малам алекум", "всем хай", "всем привет", "я новенький", "я пришел", "я вернулся", "приветули"] # создаес список "пока" слов
+
+forbidden_words = ["нигер", "нига", "пидор", "ебал", "говноед", "мразь" ]  # you can add as many words as you like, but make sure they are all lower case
+lox = ["лох", "лошара", "бля", "даун", "дебил", "дибил" ]
+help = ["?help", "?what", "?pomogi", "?помощь", "?помоги", ]
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}') # выводим информцию о успешном подключении бота
+async  def  on_message(msg):
+    if msg.content == "?help":
+        await message.channel.send(f' Список слов за которые дается мут (5 часов)` "нигер", "нига", "пидор", "ебал", "говноед", "мразь"` {message.author.mention} ')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:  # ignores itself
+        return
+    if message.author.bot == True:  # ignores other bots
+        return
+    if message.guild == None:  # ignores dms
+        return
+
+    for word in forbidden_words:
+        if word in message.content.lower():
+            await message.delete()
+            await message.author.add_roles(get(message.guild.roles, id=846083325672161332),reason=f"He said a forbidden word")
+            await bot.get_channel(846050353899765771).send(f"Message was deleted in {message.channel.mention} by {message.author.mention}:\n`{message.content}`")
+            await asyncio.sleep(18000)
+            await message.author.remove_roles(get(message.guild.roles, id=846083325672161332), reason=f"Mute has expired")
+
+    # print(msg_list)
+    # print(hello)
+    # print(list(set(msg_list + hello)))
+    # print(len(set(msg_list + hello)))
+
+        msg = message.content.lower()
+        msg_list = msg.split()
+
+
+
+
+
+    find_hello = False
+    for item in hello:
+        if msg.find(item) >= 0:
+            find_hello = True
+    if (find_hello):
+        await message.channel.send(f'  Привет {message.author.mention} !') # рекация на "привет" слово
+    find_bye = False
+    for item in bye:
+        if msg.find(item) >= 0:
+            find_bye = True
+    if (find_bye):
+        await message.channel.send(f'  Пока {message.author.mention} !') # реакция на "пока" слово
+
+    find_lox = False
+    for item in lox:
+        if msg.find(item) >= 0:
+            find_lox = True
+    if (find_lox):
+        await message.channel.send(f'  не матюкайся{message.author.mention} !') # рекация на "матюк" слово
+    find_help = False
+    for item in help:
+        if msg.find(item) >= 0:
+            find_help = True
+    if (find_help):
+        await message.channel.send(f' Список слов за которые дается мут (5 часов)` "нигер", "нига", "пидор", "ебал", "говноед", "мразь"` ')
+
+
+
+
+
+bot.run('ODQ0NjIzNTQzMzc5MTY1MjQ1.YKVG-Q.U15lhzUH1mbddneDrwltqIYQO8U')
+token - os.environ.get('BOT_TOKEN')
+bot.run(str(token))
+# {message.author.mention}
+
+
+
+
+
+# import discord
+# from discord.ext import commands
+#
+# bot = commands.Bot(command_prefix='>')
+#
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send('pong')
+#
+# bot.run('ODQ0NjIzNTQzMzc5MTY1MjQ1.YKVG-Q.U15lhzUH1mbddneDrwltqIYQO8U')
