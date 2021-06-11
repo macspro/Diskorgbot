fee8ace3c01ca7ca5292a08a7efceb4efbb7ae29
macspro <61458872+macspro@users.noreply.github.com>
2021-06-11 17:28:46 +0300

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -72,8 +72,8 @@ for item in hello:
    
          if msg.find(item) >= 0:
              find_hello = True
-    if (find_hello):
-         await message.channel.send(f'  Привет {message.author.mention} !') # рекация на "привет" слово
+         if (find_hello):
+             await message.channel.send(f'  Привет {message.author.mention} !') # рекация на "привет" слово
     find_bye = False
     for item in bye:
         if msg.find(item) >= 0:
